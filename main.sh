742931306b0e95fa3e3d2901d463aaff7823cc8b
nzy142625 <nzy142625@gmail.com>
2021-04-12 08:30:42 +0800

    init

---
+++ main.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+bvid=$(echo $1 | sed 's/\.ass//g')
+cid=$(curl -s -G "http://api.bilibili.com/x/player/pagelist" \
+--data-urlencode "bvid=$bvid" | jq .data[0].cid)
+declare -A dic
+while read line
+do
+    type=$(echo $line | awk -F, '{print $1}')
+    dic[$type]=$(echo $line | awk -F, '{print $2}')
+done < color.txt
+code=-1
+filename=$1
+cat $filename | grep 'Dialogue' | awk -F, '{printf "%s:%s:%s\n",$2,$4,$10}' | while read line
+do
+    h=$(echo $line | awk -F: '{print $1}')
+    m=$(echo $line | awk -F: '{print $2}')
+    cs=$(echo $line | awk -F: '{print $3}' | sed 's/\.//g')
+    color=${dic[$(echo $line | awk -F: '{print $4}')]}
+    msg=$(echo $line | awk -F: '{print $5}')
+    t=`expr $h \* 3600000 + $m \* 60000 + $cs \* 10 + 300`
+    rnd=$((`date '+%s'`*1000000+`date '+%N'`/1000))
+    # echo -e "$msg\n$cid\n$bvid\n$t\n$color\n$rnd\n"
+    code=$(curl -s "http://api.bilibili.com/x/v2/dm/post" \
+    --data-urlencode "type=1" \
+    --data-urlencode "oid=$cid" \
+    --data-urlencode "msg=$msg" \
+    --data-urlencode "bvid=$bvid" \
+    --data-urlencode "progress=$t" \
+    --data-urlencode "color=$color" \
+    --data-urlencode "pool=0" \
+    --data-urlencode "mode=4" \
+    --data-urlencode "rnd=$rnd" \
+    --data-urlencode "csrf=d9dd9034d238f6386206e2373a6b48f0" \
+    -b "SESSDATA=6f251ee0%2C1630911102%2C39734%2A31" | jq .code)
+    echo -e "$t\t$code\t$color\n$msg"
+    while (($code != 0))
+    do
+        sleep 5m
+        code=$(curl -s "http://api.bilibili.com/x/v2/dm/post" \
+        --data-urlencode "type=1" \
+        --data-urlencode "oid=$cid" \
+        --data-urlencode "msg=$msg" \
+        --data-urlencode "bvid=$bvid" \
+        --data-urlencode "progress=$t" \
+        --data-urlencode "color=$color" \
+        --data-urlencode "pool=0" \
+        --data-urlencode "mode=4" \
+        --data-urlencode "rnd=$rnd" \
+        --data-urlencode "csrf=d9dd9034d238f6386206e2373a6b48f0" \
+        -b "SESSDATA=6f251ee0%2C1630911102%2C39734%2A31" | jq .code)
+        echo -e "$t\t$code\t$color\n$msg"
+    done
+    sleep 10s
+done
